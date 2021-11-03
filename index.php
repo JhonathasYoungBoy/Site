359b4fc05eb05e92c5cebe582afbcca41da3e276
Jhonathas-PC <Jhonathas.souza351@gmail.com>
2021-11-02 23:05:29 -0300

    leve correcao

--- index.php
+++ index.php
@@ -148,7 +148,7 @@
     
     <footer>
         <div class="center">
-            <p>Todos os direitos reservados!</p>
+            <p>Todos os direitos reservados</p>
         </div><!--center--> 
     </footer><!--Rodapé--> 
 </body>
